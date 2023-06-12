ff9ea435f3de7a744faeaeef8b1a6a3db9c3de6b
Ravi Kant <iRvhny@gmail.com>
2023-06-13 00:24:48 +0530

    perf (home): home.views and home/index.html

--- home/views.py
+++ home/views.py
@@ -1,8 +1,17 @@
 from django.shortcuts import render
+from courses.models import Course
+
 
 # Create your views here.
 def index(request):
-    return render(request, 'home/index.html')
+
+    courses = Course.objects.all().order_by('-created_on')[:6]
+
+    context = {
+        'courses': courses
+    }
+
+    return render(request, 'home/index.html', context)
 
 
 # def contact(request):
